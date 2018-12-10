b15de10b97160feec2af5f232ed1c8b84dcd8405
Glen Sale and Leilani Leach <leilani@>
2018-12-10 13:14:32 -0800

    it will inialize new project airline planner

---
+++ AirlinePlanner/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AirlinePlanner.Models;
+
+namespace AirlinePlanner.Controllers
+{
+    public class HomeController : Controller
+    {
+    }
+}
